a711084360968a9faa6dc264d67cc284a8834eae
Omar Ismail <65664371+unworthydata@users.noreply.github.com>
2021-04-06 15:56:14 +0400

    Update Staff_store.sql
    fixed spacing

--- MySQL/Staff_store.sql
+++ MySQL/Staff_store.sql
@@ -1,25 +1,25 @@
 CREATE TABLE staff_store (
 	store_id INTEGER,
-    manager_staff_id INTEGER,
-    PRIMARY KEY (store_id, manager_staff_id),
+	manager_staff_id INTEGER,
+	PRIMARY KEY (store_id, manager_staff_id),
     
-    CONSTRAINT store_staff_store_store_id
+	CONSTRAINT store_staff_store_store_id
 		FOREIGN KEY (store_id) REFERENCES store(store_id)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT,
+		ON UPDATE CASCADE
+		ON DELETE RESTRICT,
         
 	CONSTRAINT store_staff_staff_manager_staff_id
 		FOREIGN KEY (manager_staff_id) REFERENCES staff(staff_id)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+		ON UPDATE CASCADE
+        	ON DELETE RESTRICT
 );
 
 INSERT INTO staff_store
-	SELECT 
+	SELECT
 		staff.store_id, 
-        store.manager_staff_id
-	FROM staff
-    JOIN store USING (store_id);
-    
+		store.manager_staff_id
+		FROM staff
+	JOIN store USING (store_id);
+	
 ALTER TABLE store DROP COLUMN manager_staff_id;
 ALTER TABLE staff DROP COLUMN store_id;
